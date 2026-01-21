a514841f537c71b2f338fb5c644501ab4d664dd4
PacoOrtegaUMA <pacoortega@uma.es>
2026-01-21 12:32:20 +0100

    Update Sc_ver_reglas.bat

--- Scripts/Sc_ver_reglas.bat
+++ Scripts/Sc_ver_reglas.bat
@@ -1,19 +1,20 @@
 @echo off
+REM Lanzar este propio script en una ventana MAXIMIZADA y terminar este proceso
 
-rem ===== Relanzar maximizado si no lo está =====
-if "%1" NEQ "MAX" (
-    start "" /MAX "%~f0" MAX
-    exit
-)
+start "" /MAX cmd /c "%~f0 run"
+exit /b
 
-rem ===== Variables =====
+
+:run
+REM ===== Aquí empieza lo que realmente se ejecuta =====
+
+rem Ver reglas de la cadena OUTPUT en la VM
 set "HOST=localhost"
 set "PORT=2222"
 set "USER=profesor"
 set "KEY=%~dp0Clave_Profesor_VM"
 set "SCRIPT=%~f0"
 
-rem ===== Comprobar clave =====
 if not exist "%KEY%" (
     echo ERROR: No se encuentra la clave privada: "%KEY%"
     echo.
@@ -22,27 +23,24 @@ if not exist "%KEY%" (
     exit /b 1
 )
 
-rem ===== Ejecutar SSH =====
 set "SSHO= -o BatchMode=yes -o PasswordAuthentication=no -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL"
 
 echo Mostrando reglas de la cadena OUTPUT...
 ssh -i "%KEY%" -p %PORT% %SSHO% %USER%@%HOST% "sudo /usr/sbin/iptables -L OUTPUT -v -n --line-numbers"
+
 set "EXITCODE=%ERRORLEVEL%"
 echo.
 echo EXITCODE SSH: %EXITCODE%
 echo.
 
-rem ===== Borrar clave privada =====
+echo Borrando clave privada...
 del /f /q "%KEY%"
 
-rem ===== Autoeliminar script =====
 echo Autodestruyendo script...
 start "" /min cmd /c del "%SCRIPT%"
 
-rem ===== Mostrar pausa para ver salida =====
 echo.
 echo Pulsa una tecla para cerrar...
 pause >nul
 
-rem ===== Cerrar =====
 exit /b %EXITCODE%
