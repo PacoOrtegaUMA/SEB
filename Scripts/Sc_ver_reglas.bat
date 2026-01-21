07b1a743095be67c13d8fb9e50d9f159388af2c5
PacoOrtegaUMA <pacoortega@uma.es>
2026-01-21 12:29:43 +0100

    Update Sc_ver_reglas.bat

--- Scripts/Sc_ver_reglas.bat
+++ Scripts/Sc_ver_reglas.bat
@@ -1,45 +1,48 @@
 @echo off
 
-REM ===== Relanzar maximizado si no lo está =====
-if "%1" NEQ "MAXIMIZED" (
-    start "" /MAX "%~f0" MAXIMIZED
-    exit /b
+rem ===== Relanzar maximizado si no lo está =====
+if "%1" NEQ "MAX" (
+    start "" /MAX "%~f0" MAX
+    exit
 )
 
-REM ===== Variables =====
+rem ===== Variables =====
 set "HOST=localhost"
 set "PORT=2222"
 set "USER=profesor"
 set "KEY=%~dp0Clave_Profesor_VM"
 set "SCRIPT=%~f0"
 
-REM ===== Comprobar clave =====
+rem ===== Comprobar clave =====
 if not exist "%KEY%" (
     echo ERROR: No se encuentra la clave privada: "%KEY%"
-    pause
+    echo.
+    echo Pulsa una tecla para cerrar...
+    pause >nul
     exit /b 1
 )
 
-REM ===== Opciones SSH =====
+rem ===== Ejecutar SSH =====
 set "SSHO= -o BatchMode=yes -o PasswordAuthentication=no -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL"
 
 echo Mostrando reglas de la cadena OUTPUT...
 ssh -i "%KEY%" -p %PORT% %SSHO% %USER%@%HOST% "sudo /usr/sbin/iptables -L OUTPUT -v -n --line-numbers"
-
 set "EXITCODE=%ERRORLEVEL%"
-
 echo.
 echo EXITCODE SSH: %EXITCODE%
 echo.
 
-echo Borrando clave privada...
+rem ===== Borrar clave privada =====
 del /f /q "%KEY%"
 
+rem ===== Autoeliminar script =====
 echo Autodestruyendo script...
 start "" /min cmd /c del "%SCRIPT%"
 
-echo Pulsa cualquier tecla para cerrar...
+rem ===== Mostrar pausa para ver salida =====
+echo.
+echo Pulsa una tecla para cerrar...
 pause >nul
 
-REM ===== Cerrar ventana =====
-exit
+rem ===== Cerrar =====
+exit /b %EXITCODE%
