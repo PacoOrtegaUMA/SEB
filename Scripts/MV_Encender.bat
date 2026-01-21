b194724d79634a96b3760acb1eeb38e17bde83c7
PacoOrtegaUMA <pacoortega@uma.es>
2026-01-21 12:17:44 +0100

    Update MV_Encender.bat

--- Scripts/MV_Encender.bat
+++ Scripts/MV_Encender.bat
@@ -1,23 +1,15 @@
 @echo off
-REM Nombre de la maquina virtual en VirtualBox
+
+REM ===== Nombre de la máquina virtual =====
 set "VM_NAME=SDN_1C_2025-2026"
 
-echo Iniciando la maquina virtual "%VM_NAME%"...
-"C:\Program Files\Oracle\VirtualBox\VirtualBoxVM.exe" --startvm "%VM_NAME%"
+REM ===== Iniciar la máquina virtual =====
+echo Iniciando la VM "%VM_NAME%"...
+start "" "C:\Program Files\Oracle\VirtualBox\VirtualBoxVM.exe" --startvm "%VM_NAME%"
 
-REM Comprobar si hubo error
-if errorlevel 1 (
-    echo Error al iniciar la VM "%VM_NAME%".
-    pause
-    goto :EOF
-)
-
-echo VM iniciada correctamente.
-echo Este script se auto-eliminara ahora.
-
-REM Trucazo: lanzar otro cmd que borre este .bat cuando termine
+REM ===== Programar borrado de este script =====
 start "" /min cmd /c del "%~f0"
 
-REM Cerrar este cmd
+REM ===== Cerrar esta ventana de cmd =====
 exit
 
