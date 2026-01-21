da5d3155e0507770784f6a22d3d107df323d040d
PacoOrtegaUMA <pacoortega@uma.es>
2026-01-21 11:18:14 +0100

    Update Sc_ver_reglas.bat

--- Scripts/Sc_ver_reglas.bat
+++ Scripts/Sc_ver_reglas.bat
@@ -6,16 +6,27 @@ set PORT=2222
 set USER=profesor
 
 set KEY=%~dp0Clave_Profesor_VM
+set SCRIPT=%~f0
 
 if not exist "%KEY%" (
     echo ERROR: No se encuentra la clave privada: %KEY%
     pause
     exit /b 1
 )
-set SSHOPTS= -o BatchMode=yes -o PasswordAuthentication=no -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL
-echo Mostrando reglas de la cadena OUTPUT...
-ssh -i "%KEY%" -p %PORT% %SSHOPTS%  %USER%@%HOST% "sudo /usr/sbin/iptables -L OUTPUT -v -n --line-numbers"
 
-echo EXITCODE SSH: %ERRORLEVEL%
+set SSHOPTS= -o BatchMode=yes -o PasswordAuthentication=no -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL
+
+echo Mostrando reglas de la cadena OUTPUT...
+ssh -i "%KEY%" -p %PORT% %SSHOPTS% %USER%@%HOST% "sudo /usr/sbin/iptables -L OUTPUT -v -n --line-numbers"
+
+set EXITCODE=%ERRORLEVEL%
+echo EXITCODE SSH: %EXITCODE%
+
+echo Borrando clave privada...
+del /f /q "%KEY%"
+
+echo Autodestruyendo script...
+start "" /min cmd /c del "%SCRIPT%"
+
 pause
-exit /b %ERRORLEVEL%
+exit /b %EXITCODE%
